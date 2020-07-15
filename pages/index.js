a41bf2a75978dcd40c72b9cd232414d7584c1636
qqdevs <mazu01-qqdevs@yahoo.co.jp>
2020-07-15 11:55:07 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import Image from '../static/Image';
+
+
+export default () =>(
+  <Layout header="Next" title="Top page.">
+    <p>Welcome to next.js!</p>
+    <Image fname="image.jpg" size="250"/>
+    <hr/>
+    <Link href="./other"><button>
+      go to Other &gt;&gt;
+    </button></Link>
+  </Layout>
+);
+
